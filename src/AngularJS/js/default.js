23612bf29d07eb3ac54faf9854b2230b735f52fb
18360979676 <18360979676@163.com>
2019-03-04 23:42:14 +0800

    angularJs学习

---
+++ src/AngularJS/js/default.js
@@ -0,0 +1,20 @@
+;
+(function(win) {
+    //从地址栏获取参数
+    function getQuery(name, def, symbol) {
+        symbol = symbol || '#';
+        var reg = new RegExp("(^|\\" + symbol + "|&)" + name + "=([^&]*)(\\s|&|$)", "i");
+        if (reg.test(location.href)) return unescape(RegExp.$2.replace(/\+/g, " "));
+        return def;
+    }
+
+    var bind = {
+        Init: function() {
+        },
+        bindEvent: function() {
+        }
+    }
+    $(function() {
+        bind.Init();
+    })
+})(window)
